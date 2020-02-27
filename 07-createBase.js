7545d9b3de5ae624854cba29ced52ea11b0ad92f
Ashcoca <ashvincoca@gmail.com>
2020-02-27 07:36:15 -1000

    Added day 4 problem

---
+++ 07-createBase.js
@@ -0,0 +1,27 @@
+/*
+Write a function that allows you to establish a base number and add a passed in value to the base with each invocation.
+
+Example 1:
+var addSix = createBase(6);
+
+addSix(10); // returns 16
+addSix(21); // returns 27
+
+Example 2:
+var addSeven = createBase(7);
+
+addSeven(10); // returns 17
+addSeven(21); // returns 28
+*/
+
+var createBase = function(baseNumber) {
+  // YOUR CODE HERE
+};
+
+// // Uncomment and copy into Chrome dev console to test your function!
+// var addSix = createBase(6);
+// addSix(10); // returns 16
+
+// var addTwenty = createBase(20);
+// addTwenty(5); // returns 25
+
