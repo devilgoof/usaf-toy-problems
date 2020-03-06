5e4ab4a1bb4c2025d74d7fb831d5787adf724f53
Ashcoca <ashvincoca@gmail.com>
2020-03-06 07:25:24 -1000

    Adding fizz buzz

--- 07-createMap.js
+++ 07-createMap.js
@@ -18,8 +18,21 @@ myMap([1, 2, 3], addOne);
 --> Returns [2, 3, 4]
 
 */
+// Return an array containing the results of applying a callback to each element in an array.
+
+// Does this sound familiar? It should. Your job is basically to build the array method MAP.
 
 const myMap = (array, callback) => {
-  // YOUR CODE HERE
+  // Create a copy of the input array
+  var output = [];
+  array.forEach(item => output.push(callback(item)))
+  return output;
 };
 
+// Example callback function
+var addOne = (num) => {
+  return num + 1;
+};
+
+// Demo
+console.log(myMap([1, 2, 3], addOne));
---
+++ 08-fizzBuzz.js
@@ -0,0 +1,19 @@
+/*
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+N will never be less than 1.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value 'Fizz' instead
+If the value is a multiple of 5: use the value 'Buzz' instead
+If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+
+*Hint* If a number is a multiple of 3, then what number is it also divisible by?
+
+Example:
+fizz(1, 5)
+--> [ 1, 2, 'Fizz', 4, 'Buzz' ]
+
+fizz(14, 21)
+--> [ 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz' ]
+*/
