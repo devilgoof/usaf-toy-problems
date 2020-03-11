e24ad8075c355f68623db897ea345c9376b7a6ec
Ashcoca <ashvincoca@gmail.com>
2020-03-11 07:30:17 -1000

    Adding new problem set

---
+++ 10-mathSet.js
@@ -0,0 +1,45 @@
+/*
+Today you have a series of Math related toy problems to solve.
+
+Try to get through as many as you can!
+*/
+
+// Given the length and width or a rectangle, calculate its area
+const computeAreaOfARectangle = (length, width) => {
+  // your code here
+}
+
+
+// Given a number and an exponent, return the given number, raised to the given exponent.
+const  computePower = (num, exponent) => {
+  // your code here
+}
+
+
+/*
+Given the pre tax and pre tip amount of a meal, return the total amount due after tax and tip.
+
+Notes:
+Assume that sales tax is 9.5% and tip is 15%.
+Do NOT tip on the sales tax, only on the pre tip amount.
+
+EXAMPLE:
+calculateBillTotal(20);
+// --> 24.9
+*/
+const calculateBillTotal = (preTaxAndTipAmount) => {
+  // your code here
+}
+
+
+/*
+Given a principal, an interest rate, a compounding frequency, and a time (in years)
+returns the amount of compound interest generated.
+
+EXAMPLE:
+computeCompoundInterest(1500, .043, 4, 6);
+// --> 438.8368221341061
+*/
+const computeCompoundInterest = (principal, interestRate, compoundingFrequency, timeInYears) => {
+  // your code here
+}
