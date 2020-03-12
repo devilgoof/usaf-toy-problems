bc1530914c1195846ce16eadda1d83e5ea47495d
Ashcoca <ashvincoca@gmail.com>
2020-03-12 07:25:38 -1000

    Adding array problems

---
+++ 11-arrayMethods.js
@@ -0,0 +1,55 @@
+// Don't worry about solving all of these problems, don't feel rushed! Do what you can!
+// Hint - There are array methods that may make it easier to solve these problems...
+
+// 1. Given an array and an integer return the element at the given integer, within the given array.
+// getNthElement([1, 3, 5], 1)
+// --> 3
+
+const getNthElement = (array, num) => {
+  // your code here
+};
+
+
+// 2. Given three arrays, return an array with the elements of "arr1" in order, followed by the elements in "arr2", and then "arr3".
+// joinArrays([1, 2], [3, 4], [5, 6]);
+// --> [1, 2, 3, 4, 5, 6]
+
+const joinArrays = (arr1, arr2, arr3) => {
+  // your code here
+};
+
+
+// 3. Given an array and a index, return an array with all the elements up until, but not including, the element at the given index.
+// getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3)
+// --> ['a', 'b', 'c']
+
+const getElementsUpTo = (array, index) => {
+  // your code here
+};
+
+
+// 4. Given an array and an index, return an array with all the elements but the nth.
+// getAllElementsButNth(['a', 'b', 'c'], 1);
+// --> ['a', 'c']
+
+const getAllElementsButNth = (array, index) => {
+  // your code here
+};
+
+
+// 5. Given an array of strings, return an array containing only the elements of the given array whose lengths are odd numbers.
+// filterOddLengthWords(['there', 'it', 'is', 'now']);
+// --> ['there', "now']
+
+const filterOddLengthWords = (array) => {
+  // your code here
+};
+
+
+// 6. Given an array of numbers, return a new array where each element is the square of the element of the given array.
+// squareElements([1, 2, 3]);
+// --> [1, 4, 9]
+
+const squareElements = (array) => {
+  // your code here
+};
